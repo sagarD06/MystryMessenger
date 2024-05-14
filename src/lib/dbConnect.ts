6142cf3175911e25c27320c07fc5b5fc271bd3ef
Sagar D <83652234+sagarD06@users.noreply.github.com>
2024-05-14 17:13:54 +0530

    Add Connect Db functionality.

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+  isConnected?: number;
+};
+
+const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("Db is already connected!");
+    return;
+  }
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI || "");
+    connection.isConnected = db.connections[0].readyState;
+    console.log("Mongo Db is connected!");
+  } catch (error) {
+    console.log("Mongo Db connection failed!", error);
+    process.exit(1);
+  }
+}
+
+export default dbConnect;
